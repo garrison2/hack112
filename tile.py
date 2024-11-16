8051638d7836a388a6e48de064cdbacf6a90ced3
Vishwa <vishwasu@andrew.cmu.edu>
2024-11-15 21:56:22 -0500

    tile more class build 0

--- tile.py
+++ tile.py
@@ -1,2 +1,10 @@
+
 class tile:
-    pass
+    def __init__(self):
+        self.type = 0 # some sort of object 
+        self.viewable = True
+    def drawTile(self):
+        if self.viewable:
+           #  draw(self.type)
+    def setType(self, newType):
+        self.type = newType 
