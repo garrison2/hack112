99ef9dad42049cb6c15dac972aa7bc673dbdd7ee
fdelys1 <etsy.fujgc@slmail.me>
2024-11-16 12:19:26 -0500

    added tripwire class

---
+++ tileTypes/Tripwire.py
@@ -0,0 +1,15 @@
+from cmu_graphics import *
+
+class Tripwire:
+    def __init__(self, orientation):
+        self.color = 'red'
+        self.orientation = orientation
+    
+    def __repr__(self):
+        return 'Tripwire()'
+    
+    def drawTile(self, x, y, length):
+        if self.orientation == 'horizontal':
+            drawLine(x, y + length / 2, x + length, y + length / 2, fill = 'red', lineWidth=length/10)
+        elif self.orientation == 'vertical':
+            drawLine(x + length / 2, y, x + length / 2, y + length, fill = 'red', lineWidth=length/10)
