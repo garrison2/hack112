c9d0fe121f641b168ad18d4a1cf4f47a773a3e38
helloZaku <eric.linyt@gmail.com>
2024-11-15 23:09:27 -0500

    tilecomplete

--- tile.py
+++ tile.py
@@ -1,10 +1,20 @@
 from tile_types.floor import *
-class tile:
-    def __init__(self):
+class Tile:
+    def __init__(self,x,y,length):
         self.type = Floor # some sort of object 
         self.viewable = True
-    def drawTile(self):
-        if self.viewable:
-           #  draw(self.type)
+        self.x = x
+        self.y = y
+        self.length = length
+        self.character = None
+
+
     def setType(self, newType):
         self.type = newType 
+    
+    def draw(self):
+        self.type.drawTile(self.x,self.y,self.length)
+        if self.character != None:
+            character.draw(self.x,self.y)
+
+    
