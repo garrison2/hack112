e6aad186c87fd23e59556a95a7a12073bc1045ab
vishys19 <vishwasu@andrew.cmu.edu>
2024-11-15 22:47:51 -0500

    Delete tileTypes/floor.py

--- tileTypes/floor.py
+++
@@ -1,8 +0,0 @@
-class Floor:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self. color = 'brown'
-    def drawTile(self, length):
-        drawRect(self.x, self.y, length, length, color =self.color)
-
