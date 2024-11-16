1f057bf254b3d4f0106cb253986b2ff3fafed81e
Vishwa <vishwasu@andrew.cmu.edu>
2024-11-15 22:44:51 -0500

    classes

--- tileTypes/door.py
+++ tileTypes/door.py
@@ -2,7 +2,9 @@ class Door:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self. color = 'brown'
+        self. color = 'grey'
     def drawTile(self, length):
-        drawRect(self.x, self.y, length, length)
+        drawRect(self.x, self.y, length, length, color =self.color)
+        drawline(self.x + length/2, self.y, self.x + length/2, self.y + length)
+        drawline(self.x, self.y + length/2, self.x + length, self.y + length/2)
 
