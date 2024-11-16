61f9eaab6fe2293db39f670f2ab4f6eac8293509
Vishwa <vishwasu@andrew.cmu.edu>
2024-11-16 01:29:03 -0500

    sprites

---
+++ spriteTypes/ta.py
@@ -0,0 +1,17 @@
+from PIL import image
+
+class ta:
+    philip = "phillip.png"
+    varun = "varun.png"
+    dragon3 = "dragon3.png"
+    def __init__(self, x, y, taName):
+        self.x = x
+        self.y = y  
+        self.image = ta.makeCMUImage(f'{taName}.png')
+
+    def makeCMUImage(name):
+        image = imageFolder.open(f"images/{name}")
+        return CMUImage(image)
+
+    def drawSprite(self):
+        drawImage(self.image, self.x, self.y, self.width, self.height)
